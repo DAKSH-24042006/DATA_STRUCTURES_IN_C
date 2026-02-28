12919eb5f9ded71a7822172dab8689e4bdeb2c15
DAKSH-24042006 <daksh1.mitmpl2024@learner.manipal.edu>
2026-02-28 14:40:12 +0530

    Add files via upload
    WEEK_1

---
+++ question_2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    printf("Enter the number of elements \n");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter the elements : \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+    printf("Enter the key : \n");
+    int key;
+    scanf("%d",&key);
+
+    int first=0;
+    int last=n-1;
+    int middle=(first+last)/2;
+
+    while(first<=last){
+        if(arr[middle]>key){
+            last=middle-1;
+        }
+        else if(arr[middle]<key){
+            first=middle+1;
+        }
+        else{
+            printf("found the element at : %d",middle+1);
+            break;
+        }
+
+        middle=(first+last)/2;
+    }
+}
